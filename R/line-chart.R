2b1965fe86b576318ad66d2ec5fceeecf34bfd02
pgarnry <pg@saxobank.com>
2015-12-17 12:59:28 +0100

    Updated object documentation for line_chart function. Added functionality to function including horizontal and vertical lines, title, axis titles, scaling of x and y axis, manually setting colours on lines. If statements checking whether input object is a data.frame and grouping variable is a factor.

--- R/line-chart.R
+++ R/line-chart.R
@@ -5,24 +5,48 @@
 #' @param df data frame containing data for plotting
 #' @param y specifying column name in df that should be y variable
 #' @param x specifying column name in df that should be x variable
-#' @param flip logical indicating whether barchart should be flipped or not
 #' @param title character string specifying chart title
 #' @param y.title character string specifying y-axis title
 #' @param x.title character string specifying x-axis title
+#' @param vline numeric specifying position of vertical line
+#' @param hline numeric specifying position of horizontal line
 #' @details
 #' The ellipsis is used to pass on arguments to the grey_theme function. Primary
 #' use is to specify the legend.position to either "left", "right", "bottom" and "top".
 #' @export
 
-line_chart <- function(df, y, x = NULL, group = NULL, ...) {
+line_chart <- function(df, y, x, group = NULL, title = NULL,
+                       x.title = NULL, y.title = NULL, vline = NULL,
+                       hline = NULL, ...) {
 
-  if(is.null(x)) {
-    df$x <- 1:nrow(df)
-    x <- "x"
+  # stop if input object is not a data.frame
+  if(!is.data.frame(df)) stop("Input object has to be data.frame")
+
+  # stop if grouping variable is not a factor
+  if(!is.factor(df[, group])) stop("Grouping variable should be a factor")
+
+  # set colours based on grouping
+  if(is.null(group)) {
+    palette <- chart_colours()[1]
+  } else {
+    palette <- chart_colours()[1:nlevels(df[, group])]
+    df[, group] <- as.factor(paste(" ", df[, group], "   "))
   }
 
-  g <- ggplot(df, aes_string(x = x, y = y, group = group), environment = environment()) +
-    geom_line() +
-    grey_theme()
+  # generate ggplot
+  g <- ggplot(df, aes_string(x = x, y = y, colour = group), environment = environment()) + {
+    if(!is.null(vline)) geom_vline(xintercept = vline, color = "#fe9929", size = 1)
+    } + {
+    if(!is.null(hline)) geom_hline(yintercept = hline, color = "#fe9929", size = 1)
+    } +
+    geom_line(size = 1.2) +
+    scale_colour_manual(values = palette) +
+    ggtitle(title) +
+    labs(x = x.title, y = y.title) +
+    scale_x_continuous(expand = c(.01, 0)) +
+    scale_y_continuous(expand = c(.01, .05)) +
+    grey_theme(...)
+
+  return(g)
 
 }
