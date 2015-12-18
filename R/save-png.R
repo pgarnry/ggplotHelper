957a2c6a3892408373febf4286b6c974819f4343
pgarnry <pg@saxobank.com>
2015-12-18 13:16:59 +0100

    NAMESPACE updated with save_png function.

---
+++ R/save-png.R
@@ -0,0 +1,29 @@
+#' Save ggplots into multiple png files
+#'
+#' This function takes ggplots and save them into png images with fixed size
+#' @details The function saves png files with filename equal to the name of the ggplot object.
+#' The png image is saved with a height of 480 pixels and width scaled by the golden ratio to 777
+#' (776.66 but rounded to nearest integer)
+#' @examples
+#' g1 <- density_chart(mtcars, "mpg", "cyl")
+#' mtcars$name <- rownames(mtcars)
+#' g2 <- bar_chart(mtcars, "mpg", "name", flip = T)
+#' save_png(g1,g2)
+#' @export
+
+save_png <- function(...) {
+
+  # get names from input objects and use them to name list of plots
+  snm <- sapply(substitute(list(...)), deparse)[-1]
+  plots <- list(...)
+  names(plots) <- snm
+
+  # create png images
+  for(i in 1:length(plots)) {
+    png(file = paste(names(plots)[i], ".png", sep = ""),
+        width = 777, height = 480)
+    plot(plots[[i]])
+    dev.off()
+  }
+
+}
