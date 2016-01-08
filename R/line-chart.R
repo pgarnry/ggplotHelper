e316bd21d1a7fd51310741141a75a11f0aab8dc7
pgarnry <pg@saxobank.com>
2016-01-08 10:42:02 +0100

    Added two new function variables: x.interval and date.format. The first sets the number of intervals on x-axis if the x variable is a date class. The date.format variable lets the user to change the formatting of the date. The line_chart function now handles two new date classes: POSIXct and yearmon

--- R/line-chart.R
+++ R/line-chart.R
@@ -17,7 +17,7 @@
 
 line_chart <- function(df, y, x, group = NULL, title = NULL,
                        x.title = NULL, y.title = NULL, vline = NULL,
-                       hline = NULL, ...) {
+                       hline = NULL, x.interval = NULL, date.format = NULL, ...) {
 
   # stop if input object is not a data.frame
   if(!is.data.frame(df)) stop("Input object has to be data.frame")
@@ -39,19 +39,67 @@ line_chart <- function(df, y, x, group = NULL, title = NULL,
   if(!is.null(x.title)) x.title <- paste("\n", x.title)
   if(!is.null(y.title)) y.title <- paste(y.title, "\n")
 
-  # generate ggplot
-  g <- ggplot(df, aes_string(x = x, y = y, group = group), environment = environment()) + {
-    if(!is.null(vline)) geom_vline(xintercept = vline, color = "#fe9929", size = 1)
-    } + {
-    if(!is.null(hline)) geom_hline(yintercept = hline, color = "#fe9929", size = 1)
-    } +
-    geom_line(aes(colour = id),size = 1.2) +
-    scale_colour_manual(name = id, values = palette) +
-    ggtitle(paste(title, "\n")) +
-    labs(x = x.title, y = y.title) +
-    scale_x_continuous(expand = c(.01, 0)) +
-    scale_y_continuous(expand = c(.01, 0)) +
-    grey_theme(...)
+  # checks if x.interval is numeric and date.format is character if not NULL
+  if(!is.null(x.interval) & !is.numeric(x.interval)) stop("x.interval should be numeric")
+  if(!is.null(date.format) & !is.character(date.format)) stop("date.format should be character")
+
+  # define accepted date-time classes, chron and timeDate classes are not accepted
+  dt.classes <- c("POSIXct", "Date", "yearmon", "yearqtr")
+
+  # check whether x variable is a recognized time-based class
+  if(any(class(df[, x]) %in% dt.classes)) {
+
+    if(dt.classes[1] %in% class(df[, x])) {
+
+      # generate line chart with date class POSIXct
+      g <- ggplot(df, aes_string(x = x, y = y, group = group), environment = environment()) +
+                  geom_line(aes(colour = id), size = 1.2) +
+                  ggtitle(paste(title, "\n")) +
+                  labs(x = x.title, y = y.title) + {
+                    if(is.null(x.scale)) scale_x_datetime(date_labels = format("%Y-%m-%d %H:%M:%S")) else {
+                      scale_x_datetime(date_breaks = x.scale[1], date_labels = x.scale[2], expand = c(.01, .5))}
+                  } +
+                  scale_y_continuous(expand = c(.01, 0)) +
+                  grey_theme(...)
+
+    }
+
+    if(class(df[, x]) == "yearmon") {
+
+      if(is.null(date.format)) date.format <- "%b %Y"
+
+      if(!is.null(x.interval)) date.seq <- seq(1, length(df[,x]), x.interval)
+
+      # generate line chart with date class yearmon
+      g <- ggplot(df, aes_string(x = x, y = y, group = group), environment = environment()) +
+                  geom_line(aes(colour = id), size = 1.2) +
+                  ggtitle(paste(title, "\n")) +
+                  labs(x = x.title, y = y.title) + {
+                    if(is.null(x.interval)) scale_x_yearmon(expand = c(.01, 0)) else {
+                      scale_x_yearmon(breaks = df[date.seq, x], labels = date_format(date.format), expand = c(.01, .01))}
+                  } +
+                  scale_y_continuous(expand = c(.01, 0)) +
+                  grey_theme(...)
+
+    }
+
+  } else {
+
+    # generate simple line chart
+    g <- ggplot(df, aes_string(x = x, y = y, group = group), environment = environment()) + {
+                if(!is.null(vline)) geom_vline(xintercept = vline, color = "#fe9929", size = 1)
+                } + {
+                if(!is.null(hline)) geom_hline(yintercept = hline, color = "#fe9929", size = 1)
+                } +
+                geom_line(aes(colour = id), size = 1.2) +
+                scale_colour_manual(name = id, values = palette) +
+                ggtitle(paste(title, "\n")) +
+                labs(x = x.title, y = y.title) +
+                scale_x_continuous(expand = c(.01, 0)) +
+                scale_y_continuous(expand = c(.01, 0)) +
+                grey_theme(...)
+
+  }
 
   return(g)
 
