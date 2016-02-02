f56319e04a24dd602566d0725f0f91a5c2ddcbbd
pgarnry <pg@saxobank.com>
2016-02-02 11:53:58 +0100

    Updated documentation for box_chart function and it now supports custom legend names, sub title, minimum and maximum values on y-axis and finally whether boxes should have the same colour or not.

--- R/box-chart.R
+++ R/box-chart.R
@@ -2,33 +2,77 @@
 #'
 #' This function creates an overlay plotting a bar chart in ggplot
 #' @param df data frame containing data for plotting
+#' @param y character string specifying column name in df that should be y variable
 #' @param x character string specifying column name in df that should be x variable
-#' @param group character string setting the group variable in the data.frame
-#' @param flip logical indicating whether box chart should be flipped or not
 #' @param title character string specifying chart title
+#' @param sub.title character string specifying chart sub title
+#' @param flip logical indicating whether box chart should be flipped or not
+#' @param legend.names character string specifying legend names (default is NULL)
 #' @param y.title character string specifying y-axis title
 #' @param x.title character string specifying x-axis title
+#' @param box.colours logical indicating whether boxes should have same colours or not
+#' @param min.lim numeric setting minimum limit on y-axis
+#' @param max.lim numeric setting maximum limit on y-axis
+#' @details
+#' Legend names can be modified through a character vector with same length as
+#' number of categorical variables in the x variable. If not set, legend names
+#' will take its names from the x variable.
+#' @examples
+#' box_chart(mtcars, "mpg", "cyl", title = "Miles per gallon across cylinders",
+#' sub.title = "(Period 2000-2005)", y.title = "Miles per gallon",
+#' x.title = "Cylinders, legend.position = "none")
 #' @export
 
-box_chart <- function(df, x, group = NULL, flip = FALSE, title = NULL,
-                      y.title = NULL, x.title = NULL, ...) {
+box_chart <- function(df, y, x = NULL, title = NULL, sub.title = NULL,
+                      flip = FALSE, legend.names = NULL, y.title = NULL,
+                      x.title = NULL, box.colours = FALSE,
+                      min.lim = NULL, max.lim = NULL, ...) {
 
   # stop if input object is not a data.frame
-  if(!is.data.frame(df)) stop("Input object has to be data.frame")
+  if (!is.data.frame(df)) stop("Input object has to be data.frame")
 
-  # stop if grouping variable is not a factor
-  if(!is.factor(df[, group]) & !is.null(group)) stop("Grouping variable should be a factor")
+  # stop if variable name in data frame for values not set
+  if (is.null(y)) stop("y should correspond to a variable name in input data frame")
 
-  # set boxplot colour
-  palette <- chart_colours()[1]
+  # define grouping (as factor) and insert space in the end for pretty legend labels
+  if (is.null(x)) {
+    x <- as.factor(0)
+    palette <- chart_colours()[1]
+  } else {
+    df[, x] <- as.factor(df[, x])
+    if (box.colours){
+      palette <- chart_colours()[1:nlevels(df[, x])]
+    } else {
+      palette <- rep(chart_colours()[1], nlevels(df[, x]))
+    }
+    if (is.null(legend.names)) {
+      legend.names <- paste(" ", as.character(levels(df[, x])), "   ")
+    }
+  }
+
+  # if x and y axis titles are not NULL include line break
+  if (!is.null(x.title)) x.title <- paste("\n", x.title)
+  if (!is.null(y.title)) y.title <- paste(y.title, "\n")
+
+  # define chart title object
+  if (is.null(sub.title)) {
+    chart.title <- paste(title, "\n")
+  } else {
+    chart.title <- bquote(atop(.(title, "\n"), atop(.(sub.title), "")))
+  }
 
   # create ggplot
-  g <- ggplot(df, aes_string(y = x, x = group), environment = environment()) +
-    geom_boxplot(fill = palette, colour = "#404040", alpha = .7, size = .7, outlier.colour = "#d7191c") +
-    ggtitle(title) +
+  g <- ggplot(df, aes_string(y = y, x = x, fill = x), environment = environment()) +
+    geom_boxplot(alpha = .7, size = .7, outlier.colour = "#d7191c", fatten = 1.2) +
+    scale_fill_manual(values = palette,
+                      labels = legend.names) +
+    scale_y_continuous(limits = c(min.lim, max.lim)) +
+    ggtitle(chart.title) +
     labs(x = x.title, y = y.title) +
-    grey_theme(...) + {
-      if(flip) coord_flip()
+    grey_theme(legend.position = "none") + {
+      if (x == 0) theme(axis.text.x = element_blank())
+    } + {
+      if (flip) coord_flip()
     }
 
   return(g)
