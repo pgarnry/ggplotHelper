eb196b56ac3b1ba208186e046c4176a480fc5d3e
pgarnry <pg@saxobank.com>
2016-10-07 13:48:24 +0200

    Added more colours and a base.size option for save-png function

--- R/chart-colours.R
+++ R/chart-colours.R
@@ -4,7 +4,7 @@
 chart_colours <- function() {
 
   # colour palette in HEX
-  colours <- c("#2c7bb6", "#d7191c", "#018571", "#fe9929")
+  colours <- c("#2c7bb6", "#d7191c", "#018571", "#fe9929", "#969696", "#fa9fb5")
 
   return(colours)
 
